79b5c43ef01e3bc432bb81237b25dd9502e1ddca
Nico <negretenico@gmail.com>
2025-03-07 10:43:51 -0500

    reformatting

--- binary_search/matrix_search.py
+++ binary_search/matrix_search.py
@@ -3,14 +3,14 @@ from typing import List
 
 def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
     m = [item for sublist in matrix for item in sublist]
-    l,r = 0 , len(m)-1
-    while l<= r:
-        t = l+ (r-l)//2
+    l, r = 0, len(m) - 1
+    while l <= r:
+        t = l + (r - l) // 2
         guess = m[t]
         if guess == target:
             return True
         elif target < guess:
-            r = t-1
+            r = t - 1
         else:
-            l = t+1
+            l = t + 1
     return False
